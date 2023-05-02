d975e1c65f328e56cc43b2041a4f14f8cabe3d31
imkiwifruit <127142973+hong0503@users.noreply.github.com>
2023-05-02 12:47:25 +0800

    Update Rectangle.h

--- 11週指標地獄/Rectangle.h
+++ 11週指標地獄/Rectangle.h
@@ -8,8 +8,8 @@ private:
 public:
 	Rectangle();
 	Rectangle(double, double);
-	void setWidth(double w);
-	void setHeight(double h);
+	void setWidth(double);
+	void setHeight(double);
 	double getWidth();
 	double getHeight();
 	double getArea();
